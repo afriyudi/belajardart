fd958057081ffa043e4685b7aaa441f4d4eb6ccd
afriyudi <73121294+afriyudi@users.noreply.github.com>
2024-06-10 14:12:13 +0700

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -2,5 +2,5 @@ import 'package:gitpod_dart/gitpod_dart.dart' as gitpod_dart;
 
 main(List<String> arguments) {
   print('Hello world: ${gitpod_dart.calculate()}!');
-  print('hai how are you');
+  print('hai how are you suyanto');
 }
